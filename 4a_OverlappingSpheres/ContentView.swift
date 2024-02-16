845dad361d7a69b59258311cd81c17b10544f717
Rachelle <rrosiles@hawk.iit.edu>
2024-02-16 16:33:01 -0600

    Initial Commit

---
+++ 4a_OverlappingSpheres/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  4a_OverlappingSpheres
+//
+//  Created by PHYS 440 Rachelle on 2/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 4a_OverlappingSpheres/_a_OverlappingSpheresApp.swift
@@ -0,0 +1,17 @@
+//
+//  _a_OverlappingSpheresApp.swift
+//  4a_OverlappingSpheres
+//
+//  Created by PHYS 440 Rachelle on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct _a_OverlappingSpheresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
